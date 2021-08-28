de33cc84bff9703a40c0fa4390f3eb806fed24ec
Michael Wedd <michael.wedd@leverege.com>
2021-08-28 15:35:55 -0400

    Update config vars for port

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
+    PORT: Joi.number().default(3001),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
   })
   .unknown();
